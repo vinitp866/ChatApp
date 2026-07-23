2b8335ee1f21f083819d2c622c8efbfd954e4004
Vinit Prasad <vinitp866@gmail.com>
2026-07-23 20:22:06 +0530

    Add request logging

--- server/src/sockets/socket.ts
+++ server/src/sockets/socket.ts
@@ -1,7 +1,8 @@
 import { Server, Socket } from "socket.io";
 import jwt from "jsonwebtoken";
 import { createMessage } from "../services/message.service.js";
-export const onlineUsers = new Map<string, string>();
+
+export const onlineUsers = new Map<string, Set<string>>();
 
 interface JwtPayload {
   id: string;
@@ -24,7 +25,7 @@ export const setupSocket = (io: Server) => {
         token,
         process.env.JWT_SECRET!
       ) as JwtPayload;
-      console.log("Authenticated user:", decoded.id);
+
       socket.userId = decoded.id;
       next();
     } catch {
@@ -33,47 +34,80 @@ export const setupSocket = (io: Server) => {
   });
 
   io.on("connection", (socket: AuthenticatedSocket) => {
-  console.log("Socket connected:", socket.id, "User:", socket.userId);
-    onlineUsers.set(socket.userId!, socket.id);
-    io.emit("online-users", [...onlineUsers.keys()]);
-  console.log(` User ${socket.userId} connected (${socket.id})`);
-
-  console.log("Online Users:");
-  console.log(onlineUsers);
-  socket.on(
-  "send-message",
-  async ({
-    receiverId,
-    content,
-  }: {
-    receiverId: string;
-    content: string;
-  }) => {
-    const message = await createMessage(
-      socket.userId!,
-      receiverId,
-      content
+    console.log(
+      `✅ User ${socket.userId} connected (${socket.id})`
     );
 
-    const receiverSocketId = onlineUsers.get(receiverId);
-    console.log("Receiver socket:", receiverSocketId);
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit(
-        "receive-message",
-        message
-      );
+    // Add this socket to the user's set
+    let sockets = onlineUsers.get(socket.userId!);
+
+    if (!sockets) {
+      sockets = new Set<string>();
+      onlineUsers.set(socket.userId!, sockets);
     }
 
-    socket.emit("message-sent", message);
-  }
-);
-  socket.on("disconnect", () => {
-    onlineUsers.delete(socket.userId!);
+    sockets.add(socket.id);
+
+    // Broadcast updated online users
     io.emit("online-users", [...onlineUsers.keys()]);
-    console.log(` User ${socket.userId} disconnected`);
 
     console.log("Online Users:");
     console.log(onlineUsers);
+
+    socket.on(
+      "send-message",
+      async ({
+        receiverId,
+        content,
+      }: {
+        receiverId: string;
+        content: string;
+      }) => {
+        try {
+          const message = await createMessage(
+            socket.userId!,
+            receiverId,
+            content
+          );
+
+          const receiverSockets =
+            onlineUsers.get(receiverId);
+
+          if (receiverSockets) {
+            for (const socketId of receiverSockets) {
+              io.to(socketId).emit(
+                "receive-message",
+                message
+              );
+            }
+          }
+
+          socket.emit("message-sent", message);
+        } catch (err) {
+          console.error("Send message error:", err);
+        }
+      }
+    );
+
+    socket.on("disconnect", () => {
+      console.log(
+        `❌ User ${socket.userId} disconnected (${socket.id})`
+      );
+
+      const sockets = onlineUsers.get(socket.userId!);
+
+      if (sockets) {
+        sockets.delete(socket.id);
+
+        if (sockets.size === 0) {
+          onlineUsers.delete(socket.userId!);
+        }
+      }
+
+      io.emit("online-users", [...onlineUsers.keys()]);
+
+      console.log("Online Users:");
+      console.log(onlineUsers);
+    });
   });
-});
-}
+};
